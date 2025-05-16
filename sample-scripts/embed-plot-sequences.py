93f3c0bcc4ed1405b76fae8188cc7e66a1e32962
mabarber92 <mathew.barber@aku.edu>
2025-05-16 09:37:17 +0100

    new graphs using topics

--- sample-scripts/embed-plot-sequences.py
+++ sample-scripts/embed-plot-sequences.py
@@ -14,17 +14,24 @@ import random
 articles_dir = "../data_in/articles/"
 chosen_articles = []
 
-# Randomly select articles
-no_articles = 100
-articles = os.listdir(articles_dir)
-for i in range(no_articles):
-    chosen_articles.append(random.choice(articles))
+# Use topic model to select articles
+topics = [15, 18, 33, 28]
+topic_df = pd.read_csv("../data_out/articles-data/topic-model/topic-model.csv")
+chosen_articles = topic_df[topic_df["Topic"].isin(topics)][["file", "Topic", "topic_1", "topic_2", "topic_3", "topic_4"]].values.tolist()
+
+
+# # Randomly select articles
+# no_articles = 100
+# articles = os.listdir(articles_dir)
+# for i in range(no_articles):
+#     chosen_articles.append(random.choice(articles))
 
 input_sequences = []
 input_titles = []
+article_topic = []
 
 for article in chosen_articles:
-    article_path = os.path.join(articles_dir, article)
+    article_path = os.path.join("..", article[0])
     with open(article_path, "r", encoding='utf-8') as f:
         text = f.read()
     splits = text.split("-----")
@@ -36,6 +43,8 @@ for article in chosen_articles:
     else:
         sequence = " ".join(tokens)
     input_sequences.append(sequence)
+    topic_labels = ", ".join(article[2:])
+    article_topic.append(f"{article[1]}: {topic_labels}")
 
 
 
@@ -63,11 +72,11 @@ reduced = pca.fit_transform(embeddings)
 
 dict_list = []
 for i, sequence in enumerate(input_sequences):
-    dict_list.append({"x": reduced[i, 0], "y": reduced[i, 1], "title": str(i) + "-" + input_titles[i], "no": i })
+    dict_list.append({"x": reduced[i, 0], "y": reduced[i, 1], "title": str(i) + "-" + input_titles[i], "no": i , "topic_no": str(article_topic[i])})
 
 df = pd.DataFrame(dict_list)
-df.to_csv("seq_embedding_space-50-arts.csv")
-fig = px.scatter(df, x="x", y="y", text="no", color="title", title="100 random articles from the Gaza corpus clustered according to their sequence embeddings")
+df.to_csv("seq_embedding_space-4-tops4.csv")
+fig = px.scatter(df, x="x", y="y", text="no", color="title", title=f"{len(df)} articles from the Gaza corpus clustered according to their sequence embeddings")
 fig.update_traces(textposition='top center')
 fig.update_layout(
     font=dict(        
@@ -76,4 +85,13 @@ fig.update_layout(
 )
 
 
-fig.write_html("sequence-random-100-arts.html")
+fig.write_html("sequence-4-topics-arts4.html")
+
+fig = px.scatter(df, x="x", y="y", text="no", color="topic_no", title=f"{len(df)} articles from the Gaza corpus clustered according to their sequence embeddings, coloured according to BERTopic")
+fig.update_traces(textposition='top center')
+fig.update_layout(
+    font=dict(        
+        size=8
+    )
+)
+fig.write_html("sequence-4-topics-labelled4.html")
